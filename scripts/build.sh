89c490351dba18c7b292d6cea10d752a9ace4e5b
ErnestRC <ernest@unstable.build>
2016-07-02 15:26:30 -0700

    setup ws integration spec

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 export GIT_COMMIT_SHORT=`git rev-parse --short HEAD`
 echo "building sonicd $GIT_COMMIT_SHORT"
-sbt clean assembly && \
-  docker build -t ernestrc/sonicd:$GIT_COMMIT_SHORT .
+sbt assembly && \
+  docker build -t xarxa6/sonicd:$GIT_COMMIT_SHORT .
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,22 +2,46 @@
 export GIT_COMMIT_SHORT=`git rev-parse --short HEAD`;
 export NODE_TLS_REJECT_UNAUTHORIZED=0;
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+DOMAIN=sonicd.unstable.build
 
-run_tests() {
-  (node $DIR/../examples/nodejs/example.js)
-  local res=$?
-  printf 'EXIT CODE: %d\n' "$res"
-  return $res
+clean() {
+  [[ -d "$DIR/certs" ]] && echo "skipping rm certs folder" || rm -rf $DIR/certs;
+  [[ -z "$SONICD_CONTAINER" ]] && echo "skipping rm sonicd container" || docker rm -f $SONICD_CONTAINER;
+  [[ -z "$NGINX_CONTAINER" ]] && echo "skipping rm nginx container" || docker rm -f $NGINX_CONTAINER;
 }
 
-$DIR/build.sh && \
-  echo "Starting WS integration spec for $GIT_COMMIT_SHORT" && \
-  NGINX_CONTAINER=$(docker run -v $DIR/sonicd-nginx.conf:/etc/nginx/nginx.conf:ro -v /etc/ssl/localcerts:/etc/ssl/localcerts:ro -d --net=host nginx) && \
-  SONICD_CONTAINER=$(docker run -d -v $DIR:/etc/sonicd:ro -p 9111:9111 ernestrc/sonicd:$GIT_COMMIT_SHORT) && \
-  sleep 5; \
-  TEST_RESULT=$(run_tests) && \
-  echo "finished WS integration spec for $GIT_COMMIT_SHORT. Cleaning..." && \
-  docker rm -f $NGINX_CONTAINER; \
-  docker rm -f $SONICD_CONTAINER; \
-  echo "Result: $TEST_RESULT"
+create_certs() {
+  mkdir $DIR/certs && \
+    cd $DIR/certs && \
+    $DIR/gencert.sh $DOMAIN && \
+    cd $DIR && \
+    echo 'generated temp certs'
+}
 
+test_exit() {
+  if [ $? -ne 0 ]; then
+    echo "exit status not 0: $1"
+    exit 1
+  fi
+}
+
+trap 'clean' EXIT;
+
+$DIR/build.sh && echo "successfully ran unit tests and built xarxa6/sonicd:$GIT_COMMIT_HOSRT";
+
+[[ $? -ne 0 ]] && exit 1
+
+echo "Starting WS integration spec for $GIT_COMMIT_SHORT";
+
+create_certs;
+
+NGINX_CONTAINER=$( { docker run -v ${DIR}/nginx.conf:/etc/nginx/nginx.conf:ro -v ${DIR}/certs:/etc/ssl/localcerts:ro -d --net=host nginx; } 2>&1 );
+test_exit $NGINX_CONTAINER
+echo "deployed nginx ssl proxy container: $NGINX_CONTAINER";
+
+SONICD_CONTAINER=$( { docker run -d -v ${DIR}:/etc/sonicd:ro -p 9111:9111 xarxa6/sonicd:${GIT_COMMIT_SHORT}; } 2>&1 );
+test_exit $SONICD_CONTAINER
+echo "deployed sonicd container: $SONICD_CONTAINER. starting tests in 5s..";
+sleep 5;
+
+node ${DIR}/../examples/nodejs/example.js
