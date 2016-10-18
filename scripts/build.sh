76ef4af5d711d8c8a21675fa8a087560eac96581
ErnestRC <ernest@unstable.build>
2016-10-17 18:12:06 -0700

    added latest to docker image build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 export GIT_COMMIT_SHORT=`git rev-parse --short HEAD`
 echo "building sonicd $GIT_COMMIT_SHORT"
 sbt assembly && \
-  docker build -t xarxa6/sonicd:$GIT_COMMIT_SHORT .
+  docker build -t xarxa6/sonicd:$GIT_COMMIT_SHORT -t xarxa6/sonicd:latest .
