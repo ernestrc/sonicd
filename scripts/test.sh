175fab302b52afb01d483a0ebc04a5764fcf06eb
ernestrc <ernest@unstable.build>
2018-11-13 14:14:44 -0800

    update travis docker repo

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 export GIT_COMMIT_SHORT=`git rev-parse --short HEAD`
 echo "building sonicd $GIT_COMMIT_SHORT"
 sbt assembly && \
-  docker build -t xarxa6/sonicd:$GIT_COMMIT_SHORT -t xarxa6/sonicd:latest .
+  docker build -t ernestrc/sonicd:$GIT_COMMIT_SHORT -t ernestrc/sonicd:latest .
